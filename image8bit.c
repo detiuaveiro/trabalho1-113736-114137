f901356b945ba26a19ed120fd9e299b868d18d35
raquel <raquelvinagre@ua.pt>
2023-11-02 13:29:37 +0000

    fix de uns warnings

--- image8bit.c
+++ image8bit.c
@@ -629,6 +629,7 @@ int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) { ///
       }
     }
   }
+  return match;
 }
 
 
@@ -642,9 +643,7 @@ void ImageBlur(Image img, int dx, int dy) { ///
   // Written by us
   assert (img != NULL);
   assert (dx >= 0 && dy >= 0);
-  int size = img->width * img->height;
-  uint8 maxval = img->maxval;
-  // Criação da nova imagem
+    // Criação da nova imagem
   Image img_blurred = ImageCreate(img->width, img->height, img->maxval);
 
   // Percorrer linhas & colunas
